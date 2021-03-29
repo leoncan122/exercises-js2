c646f6d4069e4b862d73ae47a326296e6e076431
leoncan122 <leon.can122@gmail.com>
2021-03-29 17:54:12 +0200

    2. project

--- week-2/Homework/2-project/js/main.js
+++ week-2/Homework/2-project/js/main.js
@@ -1,3 +1,6 @@
+//Part 1
+// declaracion de las funciones, una por cada color
+
 function changeColorBlue() {
     let colorBgr = document.querySelector('.jumbotron')
     let colorDonateBike = document.querySelector('.buttons > .btn-primary')
@@ -31,12 +34,71 @@ function changeColorGreen() {
    
 
 }
-
+// Obtencion de los botones 
 const blueBtn = document.querySelector('#blueBtn')
 const orangeBtn = document.querySelector('#orangeBtn')
 const greenBtn = document.querySelector('#greenBtn')
 
-
+// metodo addEventlistener con callback
 blueBtn.addEventListener('click', changeColorBlue)
 orangeBtn.addEventListener('click', changeColorOrange)
 greenBtn.addEventListener('click', changeColorGreen)
+
+
+// Part 2
+let emailField = document.getElementById('exampleInputEmail1')
+let nameField = document.getElementById('example-text-input')
+let describeField = document.getElementById('exampleTextarea')
+
+let arr = [emailField, nameField, describeField]
+
+ const isValid = arr.forEach( field => field.value = '')
+
+//funcion que cambia el color de los fields
+function checkFields(event) {
+    event.preventDefault()
+    
+    if(emailField.value.length == '0' && !emailField.value.includes('@')) {
+        emailField.style.backgroundColor = 'tomato'
+    } else { 
+        emailField.value = ''
+        emailField.style.backgroundColor = 'none'
+    }
+     
+    if (nameField.value.length == '0') {
+        nameField.style.backgroundColor = 'tomato'
+    } else { 
+        nameField.value = ''
+        nameField.style.backgroundColor = 'none'
+    }
+    
+    if (describeField.value.length == '0') {
+        describeField.style.backgroundColor = 'tomato'
+    } else { 
+        describeField.value = ''
+        describeField.style.backgroundColor = 'none'
+    }
+    
+}
+
+//busqueda del boton submit 
+let submitBtn = document.querySelector('.btn.btn-primary.submit')
+
+//al boton submit addEvenlistener que llama la funcion 
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    
+    if (emailField.value.length == '0' && nameField.value.length == '0' && describeField.value.length == '0' && !emailField.value.includes("@")) {
+        checkFields(event)
+        
+    } else { 
+        emailField.value = ''
+        nameField.value = ''
+        describeField.value = ''
+
+        emailField.style.backgroundColor = 'none'
+        nameField.style.backgroundColor = 'none'
+        describeField.style.backgroundColor = 'none'
+
+        alert('Thank you for filling up the form')}
+})
