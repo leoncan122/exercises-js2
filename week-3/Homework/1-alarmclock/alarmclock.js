912a305a16354e5cdb5eaee16fca2b0375007e72
leoncan122 <leon.can122@gmail.com>
2021-04-02 19:39:07 +0200

    homework alarm finish

--- week-3/Homework/1-alarmclock/alarmclock.js
+++ week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,4 +1,28 @@
-function setAlarm() {}
+function setAlarm() {
+  const set = document.getElementById('set');
+  
+  set.addEventListener('click', (event) => {
+    event.preventDefault();
+    let seconds = document.getElementById('alarmSet').value;
+
+    let timeValue = document.getElementById('timeRemaining')
+
+    let index = seconds
+
+    let id = setInterval(function() {
+      if (index === 0 ){
+        playAlarm();
+        clearInterval(id);
+      }
+      else {
+        index -= 1;
+        timeValue.innerHTML = `Time Remaining: 00:${index}`;
+      }
+      
+    },1000)
+    
+  })
+}
 
 // DO NOT EDIT BELOW HERE
 
